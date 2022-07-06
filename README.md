239de2328532580fc150a8be433ccd7a36b18135
ndowkunda <mn057@outlook.com>
2022-07-06 16:32:48 +0100

    add routes to app component

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ asos clone to learn react and ts
 # FAQ
 1. How do I run the app?
     - cd into sample-app and run `npm start`
+
+
+# Pages
+1. localhost:3000 (list of products)
+2. localhost:3000/product (list of all products)
+3. localhost:3000/product/id (a particular product -> get by it's id)
+
+# Comments
+visit localhost:3000/product/id
+load comments on individual product page (like a review section)
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ProductList } from './components/ProductList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Product } from './components/Product';
 
 function App() {
+  
   return (
-    <div className="App">
-      <ProductList />
-    </div>
-  );
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<ProductList />}></Route>
+					<Route path="/product" element={<ProductList />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
