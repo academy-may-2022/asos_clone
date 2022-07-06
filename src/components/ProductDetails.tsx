487d6706280d826787b71a6ee12c00529fac47d5
Pablo Martínez Pérez <pablompg@pm.me>
2022-07-06 16:41:30 +0200

    Added basic functionality for productDetails

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,6 +1,51 @@
+import {IProduct} from "../models/Product";
+import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 
 export const ProductDetails = () => {
+    let [product, setProduct] = useState<IProduct | null>(null);
+
     let { id } = useParams();
-    return <h1>Product {id}</h1>
+
+    const getProduct = async () => {
+        const response = await fetch(`https://dummyjson.com/products/${id}`);
+        return await response.json();
+    }
+
+    useEffect(() => {
+        getProduct().then(product => {
+            setProduct(product);
+        });
+    }, []);
+
+    if (!product) {
+        return <h1>Loading...</h1>;
+    }
+
+    /*
+        brand: string;
+        category: string;
+        description: string;
+        discountPercentage: number;
+        id: number;
+        // Alternative way: Array<string>
+        images: string[];
+        price: number;
+        rating: number;
+        stock: number;
+        thumbnail: string;
+        title: string;
+     */
+    return <div>
+        <h1>{product.title} (ID: {product.id})</h1>
+        <p>{product.description}</p>
+        <p>{product.category}</p>
+        <p>{product.discountPercentage}</p>
+        <p>{product.price}</p>
+        <p>{product.rating}</p>
+        <p>{product.stock}</p>
+        <p>Images:</p>
+        <p>{product.images}</p>
+        <p>{product.thumbnail}</p>
+    </div>
 }
