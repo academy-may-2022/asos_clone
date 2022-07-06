61f044095ac29112602bcdc6a32b4e79c35b0bbc
Emilio Garcia <github@emiliogq.com>
2022-07-06 12:21:33 +0200

    Refactor fetching products data
    Use interface Product

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,29 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
-export const ProductList = () => {
-  const [products, setProducts] = useState([]);
-    
-useEffect(() => {
-  loadData();
-}, [])
-
-
-const loadData = async() =>{
-  const response = await fetch("https://dummyjson.com/products");
-  const data =  await response.json();
-  console.log("Log : ")
-  console.log(data)
-  setProducts(data.products)
+// type cannot be extended, interface can be
+interface Product {
+    brand: string;
+    category: string;
+    description: string;
+    discountPercentage: number;
+    id: number;
+    // Alternative way: Array<string>
+    images: string[];
+    price: number;
+    rating: number;
+    stock: number;
+    thumbnail: string;
+    title: string;
 }
 
+export const ProductList = () => {
+    // useState<Product[] | []> : We would have an array of products or an empty array
+    const [products, setProducts] = useState<Product[] | []>([]);
+
+    useEffect(() => {
+        loadData().then(setProducts);
+    }, [])
+
+
+    const loadData = async () => {
+        const response = await fetch("https://dummyjson.com/products");
+        const data: Product[] = (await response.json()).products;
+        console.log("Log : ");
+        console.log(data);
+        return data;
+    }
 
 
     return (
-      <div>
+        <div>
 
-        <div className="status">This is a Product Component</div> 
+            <div className="status">This is a Product Component</div>
 
-      </div>
+        </div>
 
     );
-  };
+};
