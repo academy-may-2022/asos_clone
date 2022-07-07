b22b631d1394131b04cae8c2a4cb7e09dfccdcb7
Emilio Garcia <github@emiliogq.com>
2022-07-07 17:59:48 +0200

    Implements a unit test case for rendering a single product

--- src/components/Product.test.tsx
+++ src/components/Product.test.tsx
@@ -3,9 +3,23 @@ import { render, screen } from '@testing-library/react';
 import {Product} from './Product';
 import {IProduct} from "./ProductList";
 
-test('it renders all the products', () => {
 
-    render(<Product product={products} />);
+
+test('it renders a single the products', () => {
+    const mockProduct:IProduct = {
+        id: 1,
+        title: "iPhone",
+        brand: "Apple",
+        description: "Test",
+        category: "Test",
+        discountPercentage: 0,
+        images: [],
+        price: 0,
+        rating: 0,
+        stock: 0,
+        thumbnail: ""
+    }
+    render(<Product product={mockProduct} />);
     const linkElement = screen.getByText(/iPhone/i);
     expect(linkElement).toBeInTheDocument();
 });
