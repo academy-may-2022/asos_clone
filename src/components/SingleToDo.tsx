05e5c43e4b969dbe07e15c9cd7d593da9bee8b19
ndowkunda <mn057@outlook.com>
2022-07-08 13:44:11 +0100

    add icons & buttons for future todo use cases

--- src/components/SingleToDo.tsx
+++ src/components/SingleToDo.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { ToDo } from "../types/toDo";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DoneIcon from "@mui/icons-material/Done";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Divider from "@mui/material/Divider";
 
 interface SingleToDoProps {
 	todo: ToDo;
@@ -10,9 +16,23 @@ interface SingleToDoProps {
 export const SingleToDo = ({ todo, todos, setTodos }: SingleToDoProps) => {
 	return (
 		<form>
-			<div>
-				<span>{todo.todo}</span>
-			</div>
+			<Stack
+				direction="row"
+				alignItems="center"
+				justifyContent="center"
+				spacing={1}
+			>
+				{todo.todo}
+				<IconButton aria-label="delete" color="primary">
+					<DeleteIcon />
+				</IconButton>
+				<IconButton aria-label="edit" color="primary">
+					<EditIcon />
+				</IconButton>
+				<IconButton aria-label="done" color="primary">
+					<DoneIcon />
+				</IconButton>
+			</Stack>
 		</form>
 	);
 };
